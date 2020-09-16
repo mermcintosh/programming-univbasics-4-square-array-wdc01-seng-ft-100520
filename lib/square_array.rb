91a66c7f654105737daa8eecb3e82bc0c43a193b
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 18:06:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,13 @@
 require"pry"
 
 def square_array(array)
-  new_array = []
-  array.each do |integer|
-  new_array << integer ** 2
+    new_array = []
+    array.each do |integer|
+    new_array << integer ** 2
+  end
+    return new_array
 end
-square_array([1,3,5,7])
-return new_array
-end
-square_array([1,3,5,7])
+  square_array([1,3,5,7])
+
 
 
