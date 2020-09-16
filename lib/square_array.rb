7bbe78d1b232f80ce09a9c80ea5aa6bcca3280b5
Meredith McIntosh <meredith@mcintosh.io>
2020-09-16 17:43:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,15 +1,6 @@
-# def square_array(array)
-#   count = 0
-#   while count
-#   p array ** 2
-# end
-# square_array([1,3,5,7])
-
-
-
-def (array)
-while array[counter] do
-	puts array[counter]
-	counter += 1
+def square_array(array)
+  count = 0
+  while count
+  p array ** 2
 end
-end ([1,2,3,4,5])
+square_array([1,3,5,7])
